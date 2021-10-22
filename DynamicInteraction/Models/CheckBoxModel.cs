c2b57d931d6d61ef5218356b27c0d0a5f743cdf4
Artur <arturdrm@hotmail.com>
2021-10-22 19:25:04 -0300

    ADDED: Graph and CheckBox Modelś

---
+++ DynamicInteraction/Models/CheckBoxModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicInteraction.Models
+{
+    public class CheckBoxModel
+    {
+        public bool IsChecked { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DynamicInteraction/ViewModels/ShellViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+using DynamicInteraction.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicInteraction.ViewModels
+{
+    public class ShellViewModel : Conductor<object>.Collection.AllActive
+    {
+        private BindableCollection<CheckBoxModel> _checkBoxModel;
+        public ShellViewModel()
+        {
+
+        }      
+
+        public BindableCollection<CheckBoxModel> CheckBoxModel
+        {
+            get 
+            { 
+                return _checkBoxModel; 
+            }
+            set 
+            { 
+                _checkBoxModel = value;
+                NotifyOfPropertyChange(() => CheckBoxModel)
+;            }
+        }
+
+    }
+}
